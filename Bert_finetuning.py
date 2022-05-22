73bc9420880bf3c5e31008675ffae6bbf6aa08f1
momozzing <ahdbsgh2@naver.com>
2022-05-23 03:36:18 +0900

    finish

--- Bert_finetuning.py
+++ Bert_finetuning.py
@@ -1,26 +1,74 @@
 import pandas as pd
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, BertForSequenceClassification
 from torch.optim import AdamW
-from kobert_tokenizer import KoBERTTokenizer
+from sklearn.metrics import label_ranking_average_precision_score
+import random
+import os 
+import wandb
 
-tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+random_seed = 1234
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed) # if use multi-GPU
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+## data
+
+train_data = pd.read_csv('data/unsmile_train_v1.0.tsv', delimiter='\t')
+
+unsmile_labels = ["여성/가족","남성","성소수자","인종/국적","연령","지역","종교","기타 혐오","악플/욕설","clean"]
+num_labels = len(unsmile_labels)
+
+labels_df = train_data[["여성/가족","남성","성소수자","인종/국적","연령","지역","종교","기타 혐오","악플/욕설","clean"]]
+labels_df = labels_df.astype(float)
+
+label = []
+
+for i in range(len(train_data["여성/가족"])):
+
+    label.append([np.array(labels_df.iloc[i], dtype=np.float64)])
+
+label_data = pd.DataFrame(label, columns = ['label'])
+
+train_data.rename(columns={'문장':'sentence'}, inplace=True)
+
+dataset = pd.concat([train_data['sentence'], label_data['label']], axis=1)
+
+
+
+
+model_name = 'beomi/kcbert-base'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 model = BertForSequenceClassification.from_pretrained(
-    'skt/kobert-base-v1',
+    model_name,
     num_labels=10,
+    problem_type="multi_label_classification"
 ).cuda()
+model.config.id2label = {i: label for i, label in zip(range(num_labels), unsmile_labels)}
+model.config.label2id = {label: i for i, label in zip(range(num_labels), unsmile_labels)}
 
-train_data = pd.read_csv("data/train_data.csv", delimiter="\t")
-train_data = train_data[:int(len(train_data)*0.8)]
-eval_data = train_data[int(len(train_data)*0.8):]
+wandb.init(project="Hate_Speach", name=f"{model_name}-finetuning")
+
+
+train_data = dataset[:int(len(dataset)*0.8)]
+eval_data = dataset[int(len(dataset)*0.8):]
 
 train_text, train_labels = (
     train_data["sentence"].values,
     train_data["label"].values,
 )
+
+# print(train_labels)
 
 dataset = [
     {"data": t, "label": l}
@@ -29,11 +77,12 @@ dataset = [
 
 train_loader = DataLoader(
     dataset,
-    batch_size=128,
+    batch_size=64,
     num_workers=8,
     drop_last=True,
     pin_memory=True,
 )
+
 
 # eval_data = pd.read_csv("data/train_data.csv", delimiter="\t")
 # eval_data = eval_data[100000:]
@@ -48,7 +97,7 @@ dataset = [
 ]
 eval_loader = DataLoader(
     dataset,
-    batch_size=128,
+    batch_size=64,
     num_workers=8,
     drop_last=True,
     pin_memory=True,
@@ -58,19 +107,17 @@ optimizer = AdamW(params=model.parameters(),
     lr=3e-5, weight_decay=3e-7
 )
 
-epochs = 1
+epochs = 20
 for epoch in range(epochs):
     model.train()
     for train in tqdm(train_loader):
         optimizer.zero_grad()
-        text, label = train["data"], train["label"]
+        text, label = train["data"], train["label"].cuda()
         tokens = tokenizer(
             text,
             return_tensors="pt",
             truncation=True,
             padding=True,
-            # max_length=140
-
         )
 
         input_ids = tokens.input_ids.cuda()
@@ -84,27 +131,29 @@ for epoch in range(epochs):
         loss = output.loss
         loss.backward()        
         optimizer.step()
-        classification_results = output.logits.argmax(-1)
-
-        acc = 0
-        for res, lab in zip(classification_results, label):
-            if res == lab:
-                acc += 1
-
-    print({"loss": loss.item()})
-    print({"acc": acc / len(classification_results)})   ## 탭하나 안에 넣으면 step단위로 볼수있음. 
+        classification_results = output.logits
+        
+        # lrap_score = 0
+        # lrap_score += label_ranking_average_precision_score(label.detach().cpu().numpy(), classification_results.detach().cpu().numpy())
+        # print(label_ranking_average_precision_score(label.detach().cpu().numpy(), classification_results.detach().cpu().numpy()))
+        # print(len(classification_results))
+    print({'train_lrap': label_ranking_average_precision_score(label.detach().cpu().numpy(), classification_results.detach().cpu().numpy())})
+    print({"train_loss": loss.item()})
+    wandb.log({"train_lrap": label_ranking_average_precision_score(label.detach().cpu().numpy(), classification_results.detach().cpu().numpy())})
+    wandb.log({"train_loss": loss.item()})
+    # print({"acc": acc / len(classification_results)})   ## 탭하나 안에 넣으면 step단위로 볼수있음. 
 
     with torch.no_grad():
         model.eval() 
         for eval in tqdm(eval_loader):
-            eval_text, eval_label = eval["data"], eval["label"]
+            eval_text, eval_label = eval["data"], eval["label"].cuda()
             eval_tokens = tokenizer(
                 eval_text,
                 return_tensors="pt",
                 truncation=True,
                 padding=True,
-                # max_length=140
             )
+
             input_ids = eval_tokens.input_ids.cuda()
             attention_mask = eval_tokens.attention_mask.cuda()
 
@@ -114,15 +163,15 @@ for epoch in range(epochs):
                 labels=eval_label
             )
 
-            eval_classification_results = eval_out.logits.argmax(-1)
             eval_loss = eval_out.loss
+            eval_classification_results = eval_out.logits
 
-            eval_acc = 0
-            for res, lab in zip(eval_classification_results, eval_label):
-                if res == lab:
-                    eval_acc += 1
-
-        print({"eval_loss": eval_loss.item()})   ## 이미 다 적용된 상태인듯..
-        print({"eval_acc": eval_acc / len(eval_classification_results)})             ## 탭하나 안에 넣으면 step단위로 볼수있음. 
+        print({'eval_lrap': label_ranking_average_precision_score(eval_label.detach().cpu().numpy(), eval_classification_results.detach().cpu().numpy())})
+        print({"eval_loss": eval_loss.item()})
         print({"epoch": epoch+1})
-        torch.save(model.state_dict(), f"model_save/BERT_fintuing_NSMC-{epoch+1}.pt")
+
+        wandb.log({"eval_lrap": label_ranking_average_precision_score(eval_label.detach().cpu().numpy(), eval_classification_results.detach().cpu().numpy())})
+        wandb.log({"eval_loss": eval_loss.item()})
+        wandb.log({"epoch": epoch+1})
+
+        torch.save(model.state_dict(), f"model_save/Hate_Speach-{model_name.replace('/', '-')}-{epoch+1}.pt")
